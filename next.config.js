80c587143739482a7ca469614218efc1c1d00968
thach3112003 <thach3112003@gmail.com>
2025-05-10 17:47:29 +0700

    Deloy again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "standalone",
+  images: {
+    domains: ["placehold.co"],
+    unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
